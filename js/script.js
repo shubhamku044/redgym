80cec1c9a9378825856007503e78b514b7b28439
shubhamku044 <shubhamku044@gmail.com>
2021-04-04 20:52:40 +0530

    this is my second commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+
+$(document).ready(function(){
+        $('.toggle_menu').click(function(){
+                $('.toggle_menu').toggleClass('active')
+                $('.navlist').toggleClass('active')
+            })
+        });
+        
+        
+        
+        
+        
+        
+        
+        
+// $(document).on('click',function(divclose){
+//         if($(divclose.target).closest("#navlist").length == 0 )
+//         (
+//             $("#navlist").hide();
+//         )
+//         });
+        
+
+
+
+
+
+
