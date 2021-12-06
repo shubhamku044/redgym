a5cc42b1d075a012cfaad83f7084b98f7180f96f
shubhamku044 <shubhamku044@gmail.com>
2021-12-06 10:51:03 +0530

    created folder structure

--- js/script.js
+++ js/script.js
@@ -1,29 +1,6 @@
-
-
-$(document).ready(function(){
-        $('.toggle_menu').click(function(){
-                $('.toggle_menu').toggleClass('active')
-                $('.navlist').toggleClass('active')
-            })
-        });
-        
-        
-        
-        
-        
-        
-        
-        
-// $(document).on('click',function(divclose){
-//         if($(divclose.target).closest("#navlist").length == 0 )
-//         (
-//             $("#navlist").hide();
-//         )
-//         });
-        
-
-
-
-
-
-
+$(document).ready(function () {
+	$('.toggle_menu').click(function () {
+		$('.toggle_menu').toggleClass('active');
+		$('.navlist').toggleClass('active');
+	});
+});
