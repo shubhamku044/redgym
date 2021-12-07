571f1d53fd2503b11a8b8244b5c8afad420d864f
shubhamku044 <shubhamku044@gmail.com>
2021-12-07 07:35:07 +0530

    fixed the hamburger button working

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-const toggle_menu = document.querySelector('#toggle');
-const navlist = document.querySelector('#navlist');
+console.log('hello world');
+
+const toggle_menu = document.querySelector('.toggle_menu');
+const navlist = document.querySelector('.navlist');
 
 toggle_menu.addEventListener('click', () => {
-	navlist.classList.toggle('active');
-	toggle_menu.classList.toggle('active');
+	if (!navlist.classList.contains('active')) {
+		navlist.classList.add('active');
+		toggle_menu.classList.add('active');
+	} else {
+		navlist.classList.remove('active');
+		toggle_menu.classList.remove('active');
+	}
 });
